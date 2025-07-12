0c49e20372b6654b00c37d2e4bee84878b768d72
TatyanaKravtsova <tanayakravtsova@gmail.com>
2025-07-12 18:25:21 +0300

    final

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,11 +5,15 @@ document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.main-header');
   const logo = document.querySelector('.main-header__logo');
   const logoImg = document.querySelector('.logo__img');
+  const map = document.querySelector('.map');
+  const contacts = document.querySelector('.contacts');
   nav.classList.remove('no-js');
   menuButton.classList.remove('no-js');
   header.classList.remove('no-js');
   logo.classList.remove('no-js');
   logoImg.classList.remove('no-js');
+  map.classList.remove('map--no-js');
+  contacts.classList.remove('contacts--no-js');
   menuButton.addEventListener('click', () => {
     nav.classList.toggle('main-header__nav--opened');
     menuButton.classList.toggle('main-header__menu-button--opened');
